645d23cf57eff74996f0784fc4cd6cddfc976f1c
Matheus Solha <mcsolha@gmail.com>
2020-02-27 15:45:13 -0300

    added missing documentation

--- src/main/java/com/open/store/open_store/core/exception/InvalidRateException.java
+++ src/main/java/com/open/store/open_store/core/exception/InvalidRateException.java
@@ -1,5 +1,11 @@
 package com.open.store.open_store.core.exception;
 
+import com.open.store.open_store.core.model.tax.AbstractSaleTax;
+
+/**
+ * Exception thrown when an invalid rate is passed to a {@link AbstractSaleTax}
+ * @author Matheus Solha
+ */
 public class InvalidRateException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/open/store/open_store/core/utils/Rounding.java
+++ src/main/java/com/open/store/open_store/core/utils/Rounding.java
@@ -1,5 +1,9 @@
 package com.open.store.open_store.core.utils;
 
+/**
+ * Contains static methods used for number rounding
+ * @author Matheus Solha
+ */
 public class Rounding {
     /**
      * Round a double value up to the nearest 0.05
