2fe5ede7b73624629c16ef4d57ed83b6062d44f9
Matheus Solha <mcsolha@gmail.com>
2020-02-26 22:10:54 -0300

    Initial commit :tada:

---
+++ src/main/java/com/open/store/open_store/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.open.store.open_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
